18a0fed060fc548d2b01a9097caf741237c5ff66
Jmo <jpayet@.sii.fr>
2015-11-23 15:58:23 +0100

    ajout de set_locale (i18n)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 ##
 # Controller de l'application
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # definit la variable locale
+  # soit le param passe dans url
+  # soit la variable du fichier de conf
+  def set_locale
+    #I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = (['fr', 'en'].include?(params[:locale])) ? params[:locale] : I18n.default_locale
+  end 
+  
+  # ajout la variable locale a url
+  # http://localhost:3000/items/new?locale=fr
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end  
 end
