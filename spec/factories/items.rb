61920250055cff1ba59382697263410cad839580
Jmo <jpayet@.sii.fr>
2015-11-25 09:13:07 +0100

    ajout user

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,14 +2,15 @@ FactoryGirl.define do
   factory :item do
     titre Faker::Lorem.word
     contenu  Faker::Lorem.paragraph
-    image Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/spongebob.jpg"), 'image/jpg')
+    image Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/spongebob.jpg"), 'image/jpeg')
+    user # association
+    # image { fixture_file_upload( 'spec/fixtures/spongebob.jpg', 'image/jpg') }
   end
-  
-    factory :item_db, class: Item do
-    titre Faker::Lorem.word
-    contenu  Faker::Lorem.paragraph
-    image Rails.root.join("/items/spongebob.jpg")
-    
-    FileUtils.cp "spec/fixtures/spongebob.jpg", "public/items/spongebob.jpg"
-  end
+
+  # factory :item_db, class: Item do
+    # titre Faker::Lorem.word
+    # contenu  Faker::Lorem.paragraph
+    # image Rails.root.join("/items/spongebob.jpg")
+    # FileUtils.cp "spec/fixtures/spongebob.jpg", "public/items/spongebob.jpg"
+  # end
 end
