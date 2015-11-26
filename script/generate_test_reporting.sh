1ee62d5b3c457262a33c14a8606c8c6441d56cb1
Jmo <jpayet@.sii.fr>
2015-11-26 14:53:21 +0100

    test script

--- script/generate_test_reporting.sh
+++ script/generate_test_reporting.sh
@@ -5,5 +5,5 @@
 
 ########## PROCESS ##########
 # integration tests
-sudo rm -f /home/jmpayet/todolist_test_$(date "+%F_%X").html
-sudo bundle exec rspec -f h spec >> /home/jmpayet/todolist_test_$(date "+%F_%X").html
+echo $(pwd)
+sudo bundle exec rspec -f h spec >> ~/todolist_test_$(date "+%F_%H-%M-%S").html
